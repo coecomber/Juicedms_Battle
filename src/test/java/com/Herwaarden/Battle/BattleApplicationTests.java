2bbfdb0c5da7813e26915e87a769e6f70e073813
coecomber <joostvherwaarden@hotmail.com>
2020-01-03 15:04:44 +0100

    Initial commit

---
+++ src/test/java/com/Herwaarden/Battle/BattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Herwaarden.Battle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
